a76888f50547fd5e554a2f6ba6b5942641b4b77d
Enrique Collazo <ecollazo1991@gmail.com>
2021-02-03 18:33:14 -0500

    updated script

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,5 @@
 $(document).ready( function() {
 
-    var cityArr = [];
-
     //Clears all local storage items and previous searches from the page.
     $(".clear").on("click", function () {
         localStorage.clear();
@@ -9,21 +7,52 @@ $(document).ready( function() {
         $("#searchInput").val("");
     });
 
+    function save (newCity){
+        var cityArr = JSON.parse(localStorage.getItem("cityArray")) || [];
+        cityArr.push(newCity);
+        localStorage.setItem("cityArray", JSON.stringify(cityArr));
+    }
+
+    function renderCityBtn (){
+        var cityArr = JSON.parse(localStorage.getItem("cityArray")) || [];
+        
+        $("#history").empty();
+
+        cityArr.forEach(citytext => {
+            console.log(citytext);
+            var li = $("<li>");
+            var button = $("<button>");
+            button.addClass("btn btn-primary history-btn").text(citytext);
+            li.append(button)
+            $("#history").append(li);
+        });
+    };
+
+    renderCityBtn();
+
+    $(".history-btn").on("click", function() {
+        var citytext = $(this).text();
+        searchWeather(citytext);
+    });
+
     $(".searchBtn").on("click", function () {
-
         var citySearched = $('#searchInput').val().trim();
+        searchWeather(citySearched);
+        save(citySearched);
+        renderCityBtn();
+    });
 
-        localStorage.setItem("city", citySearched);
+    function searchWeather(city) {
 
         var API_KEY = "b2a7361f2d32eb5699ee502bb4d55d43";
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&appid=" + API_KEY;
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
 
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
 
-            var city = response.name;
+            var cityName = response.name;
             var kTemp = response.main.temp;
             var fTemp = Math.floor(((kTemp - 273.15) * 1.8) + 32);
             var humidityNow = response.main.humidity;
@@ -31,7 +60,7 @@ $(document).ready( function() {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
 
-            $("#currentCity").text(city);
+            $("#currentCity").text(cityName);
             $("#temp").text("Temperature: " + fTemp);
             $("#humidity").text("Humidity " + humidityNow);
             $("#windSpeed").text("Wind Speed " + windSpeednow + " MPH");
@@ -47,8 +76,10 @@ $(document).ready( function() {
             
         })
 
-    });
+    };
 });
+
+
 
 
 
